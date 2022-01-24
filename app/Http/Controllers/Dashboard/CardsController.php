328a3e8be617347e60e768dbb08852ac24a686a0
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-01-24 17:10:35 +0200

    job

--- app/Http/Controllers/Dashboard/CardsController.php
+++ app/Http/Controllers/Dashboard/CardsController.php
@@ -36,7 +36,7 @@ class CardsController extends Controller
              }
          }
         }*/
-        dd($cards);
+        //dd($cards);
         $Cards = Cards::when($request->search, function ($q) use ($request) {
 
             return $q->where('card_code', '%' . $request->search . '%');
