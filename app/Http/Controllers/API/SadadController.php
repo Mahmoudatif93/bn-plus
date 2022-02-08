2fb5381384541d29a7e42f8dbe97fe08cc2621de
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-02-08 20:31:15 +0200

    national Dubai api

--- app/Http/Controllers/API/SadadController.php
+++ app/Http/Controllers/API/SadadController.php
@@ -35,7 +35,7 @@ class SadadController extends Controller
             'Accept' => 'application/json',
             'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYmI5MGU2MDdmMWM3OWM4OTg4YWNmNzQyOGEzMjE0MGE0ZWVmYzc3OWNmOWI1ZGMzZThlNGI2NDJlNzk5ZDkzNTY2Y2IyOWI0MTBhNWMzMjgiLCJpYXQiOjE2NDQzNDQ4ODgsIm5iZiI6MTY0NDM0NDg4OCwiZXhwIjoxNzcwNTc1Mjg4LCJzdWIiOiI3Iiwic2NvcGVzIjpbXX0.kOSSstA8f6ZBhRPEDDRWKmzkPzeaQKM3MzueDTMaI7-M5_iWSTnOswPRcbhbuAway1fFxR18sx2mHTLuc6GCvJmrfz8Xe2sDVY5byrt4U0GMRnUUdh-YiLELAmaK0vuhHJKlvWcYEVTN2EVgPbK3sgVoXFOp-l-p6uZ5cxVsw9HVba2LR-eQjrTlBvhg3s3OD1zeMXc-oFb6l7zXg07bLlZ4Ptmtx7ARDeO0OaGG9hFzIlm5fc3qH-9s1I1vO_FikAAd3Ne31uuxij6NpkawuxdYUTAQicX9wZIeU3BsIImrN4r6fVfxy5-tqzCnbXbfwteH4mzXcd2-zdLqyBsYtDGDjxEWFIFtdU8XMeX9m5YokhlG1epWjobrjnO_jcPs-s9Xp6gR7XcDxZZLD-2A19ViMPKNifMcHR0BZ1th3RrtnXBZ34X8yRMqQNuKp3QeliVG4rxb97S8_v4Wbz3zWEBEFjO8qx6lEPCxQyssfu76ngjpGwk9sTSPv1wGTbZIVTjL7bwTJ0xz5-qFEKzoEGILN8MMgk__16lcHBQ4cVCDsAIgzQUh62S3Q0yMxDq6Cyc580QAvzXNNrAPnwCHyJgsUYhrp07_BTl_7bwUKnMA19O_rHdPO0p5fAoGTYAoisWRc7DeM7kfHaKETWErGWDPKyTUlCAHzV3xE9m8s00',
             'X-API-KEY' => '984adf4c-44e1-418f-829b'
-        ])->post('https://api.plutus.ly/api/v1/transaction/sadadapi/verify', [
+        ])->post('https://api.sandbox.plutus.ly/api/v1/transaction/sadadapi/verify', [
             'mobile_number' => $request->mobile_number,
             'birth_year' => $request->birth_year,
             'amount' => $request->amount
