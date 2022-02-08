31b3573003c3db5e6b0e6ff068fd8b9bc56de8b3
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-02-08 20:11:23 +0200

    national Dubai api

--- app/Http/Controllers/Dashboard/CompanyController.php
+++ app/Http/Controllers/Dashboard/CompanyController.php
@@ -15,7 +15,7 @@ class CompanyController extends Controller
     {
 
         $balancenational = Http::withHeaders([
-            'Content-Type' => 'application/json'
+            'Content-Type' => 'application/x-www-form-urlencoded'
         ])->post('https://taxes.like4app.com/online/check_balance', [
             'deviceId' => '4d2ec47930a1fe0706836fdd1157a8c320dfc962aa6d0b0df2f4dda40a27b2ba',
             'email' => 'sales@bn-plus.ly',
@@ -24,7 +24,7 @@ class CompanyController extends Controller
             'langId' => 1,
         ]);
 
-        return $balancenational ;
+       // return $balancenational ;
 
         $response = Http::withHeaders([
             'Accept' => 'application/json',
