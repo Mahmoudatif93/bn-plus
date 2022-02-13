811a53e53bf81c65f66e7d5527bf0266df464953
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-02-13 20:13:17 +0200

    new api updates

--- app/Http/Controllers/Dashboard/CompanyController.php
+++ app/Http/Controllers/Dashboard/CompanyController.php
@@ -31,7 +31,7 @@ class CompanyController extends Controller
 
 return $balancenational;*/
 
-      /* $curl = curl_init();
+       $curl = curl_init();
 
 curl_setopt_array($curl, array(
   CURLOPT_URL => "https://taxes.like4app.com/online/check_balance/",
@@ -49,24 +49,16 @@ curl_setopt_array($curl, array(
     'securityCode' => '4d2ec47930a1fe0706836fdd1157a8c36bd079faa0810ff7562c924a23c3f415',
     'langId' => '1'),
 
-));*/
-
-$balancenational = Http::post('https://taxes.like4app.com/online/check_balance/', [
-    'deviceId' => '4d2ec47930a1fe0706836fdd1157a8c320dfc962aa6d0b0df2f4dda40a27b2ba',
-    'email' => 'sales@bn-plus.ly',
-    'password' => '149e7a5dcc2b1946ebf09f6c7684ab2c',
-    'securityCode' => '4d2ec47930a1fe0706836fdd1157a8c36bd079faa0810ff7562c924a23c3f415',
-    'langId' => 1,
-]);
+));
 
 
-/*
+
 $balancenational = curl_exec($curl);
 
-curl_close($curl);*/
+//curl_close($curl);
 
 if (isset($balancenational) && !empty($balancenational) && $balancenational!='error code: 1020') {
-    return $balancenational;
+    return $balancenational[0];
   /*  if ($balancenational->balance > 0) {
         return $balancenational->balance ;
     }*/
