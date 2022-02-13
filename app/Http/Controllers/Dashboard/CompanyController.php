56c11d2937139838a4276a3aa9f697bf8a8366d1
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-02-13 20:07:54 +0200

    new api updates

--- app/Http/Controllers/Dashboard/CompanyController.php
+++ app/Http/Controllers/Dashboard/CompanyController.php
@@ -56,7 +56,7 @@ $balancenational = curl_exec($curl);
 curl_close($curl);
 
 if (isset($balancenational) && !empty($balancenational) && $balancenational!='error code: 1020') {
-    return $balancenational;
+    return $balancenational['balance'];
   /*  if ($balancenational->balance > 0) {
         return $balancenational->balance ;
     }*/
