2ed1d6005793696d591bf63813444629357aaf32
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-02-13 21:40:31 +0200

    new api updates

--- app/Http/Controllers/Dashboard/CompanyController.php
+++ app/Http/Controllers/Dashboard/CompanyController.php
@@ -136,7 +136,7 @@ class CompanyController extends Controller
 
                     $cards = json_decode($cardsnational, true);
 
-                   //return $cards['data'] ;
+                   return count($cards['data']) ;
                     $cardsave = new Cards;
                     $allcardsid = array();
 
