e245600f36fcb937f98766d6a824084aae1155e6
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-02-14 20:36:49 +0200

    new api updates

--- app/Http/Controllers/Dashboard/CompanyController.php
+++ app/Http/Controllers/Dashboard/CompanyController.php
@@ -72,7 +72,7 @@ class CompanyController extends Controller
                 ));
 
                 $companiesnational = curl_exec($curl2);
-//return($companiesnational);
+return($companiesnational);
                 $national = json_decode($companiesnational, true);
                 $compsave = new Company;
                 $allcompanyid = array();
@@ -134,6 +134,7 @@ class CompanyController extends Controller
                     $cardsnational = curl_exec($curl3);
 
                     $allcards = json_decode($cardsnational, true);
+
 
                  
                     $cardsave = new Cards;
