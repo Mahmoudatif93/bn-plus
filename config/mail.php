19afd66678168a1c99cfad84e9621babafbb330b
mahmoudatif93 <mahmoudatif22@gmail.com>
2022-02-15 20:18:11 +0200

    new api updates

--- config/mail.php
+++ config/mail.php
@@ -36,7 +36,7 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
@@ -70,7 +70,6 @@ return [
         'array' => [
             'transport' => 'array',
         ],
-
     ],
 
     /*
@@ -107,8 +106,5 @@ return [
             resource_path('views/vendor/mail'),
         ],
     ],
-
-
-
 
 ];
